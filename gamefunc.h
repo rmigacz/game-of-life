7158fa40d80eeb60544fa8df087a0e8f0cdbf776
Rafal Migacz <migacz.rafal@gmail.com>
2019-11-09 19:18:49 +0100

    Improve sources and README formatting

--- gamefunc.h
+++ gamefunc.h
@@ -6,9 +6,9 @@
 #include <string.h>
 #include <time.h>
 
-int** init_map(char* filename, int map_size);
-void free_map(int** map, int map_size);
-void print_map(int** map, int map_size);
-void iterate(int** map, int map_size);
+int** init_map(char *filename, int map_size);
+void free_map(int **map, int map_size);
+void print_map(int **map, int map_size);
+void iterate(int **map, int map_size);
 
 #endif
--- gameoflife.c
+++ gameoflife.c
@@ -3,34 +3,34 @@
 #define STEPS 10000000
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-    	printf("Usage: ./game-of-life file_name map_size\n");
-    	return 1;
-    }
+	if (argc != 3) {
+		printf("Usage: ./game-of-life file_name map_size\n");
+		return 1;
+	}
 
-    char *file_name = argv[1];
-    int map_size = atoi(argv[2]);
+	char *file_name = argv[1];
+	int map_size = atoi(argv[2]);
 
-    int **map = init_map(file_name, map_size);
+	int **map = init_map(file_name, map_size);
 
-    if (map == NULL) {
-	return 2;
-    }
+	if (map == NULL) {
+		return 2;
+	}
 
-    print_map(map, map_size);
-    
-    int i = 0;
-    struct timespec reqtime;
-    reqtime.tv_sec = 0;
-    reqtime.tv_nsec = 25000000;
+	print_map(map, map_size);
 
-    while (i < STEPS) {
-	    iterate(map, map_size);
-	    nanosleep(&reqtime, NULL);
-	    print_map(map, map_size);
-	    i++;
-    }
+	int i = 0;
+	struct timespec reqtime;
+	reqtime.tv_sec = 0;
+	reqtime.tv_nsec = 25000000;
 
-   free_map(map, map_size);
-    return 0;
+	while (i < STEPS) {
+		iterate(map, map_size);
+		nanosleep(&reqtime, NULL);
+		print_map(map, map_size);
+		i++;
+	}
+
+	free_map(map, map_size);
+	return 0;
 }
